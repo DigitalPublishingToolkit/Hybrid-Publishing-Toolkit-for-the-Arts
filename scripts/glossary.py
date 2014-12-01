82210095cb74311695d5671f4fdcccd102b22432
Castro0o <andre@andrecastro.info>
2014-12-01 15:20:50 +0100

    Glossary script: fixed issues with problematic terms

--- scripts/glossary.py
+++ scripts/glossary.py
@@ -65,7 +65,9 @@ def find_term_in_text(term, gloss_file, search_dir): #find a match for each glos
             for content in p_strong:          
                 if content.text is not None:
                     text = (content.text).encode('UTF-8')
-                    if term in text or term in text.capitalize() or term in text.lower():  # BUG: PROBLEMATIC TERMS
+                    if re.search(r'^{}$'.format(term), text, re.IGNORECASE):                        
+
+#                    if term in text or term in text.capitalize() or term in text.lower():  # BUG: PROBLEMATIC TERMS
                         print 'MATCH TEXT:', text, '-', 'TERM:', term     ## Step 3.3: TEXT LINK: Add link to glossary_term                          
                         return content, xhtml_parsed, f
                         break
@@ -75,7 +77,15 @@ def strip_glossary_term(item): #remove whatever is inside parenthesis
     h5_text = h5.text
     reg_noparenthesis= re.compile(r'(.*)\s\(.*?\)')
     h5_term = re.sub(reg_noparenthesis, r'\1', h5_text)
-    return h5_term
+    if ' or ' in h5_term:
+        term_list=h5_term.split(' or ')
+        
+    elif ' / ' in h5_term:    
+        term_list=h5_term.split(' / ')
+    else:        
+        term_list=[]
+        term_list.append(h5_term)
+    return term_list
 
 def parse_html(content_dir, content_file ):
     xhtml = open(content_dir + content_file, "r")
@@ -109,36 +119,37 @@ gloss_terms = gloss_tree.findall('.//section[@class="level5"]') # find glossary 
 
 # loop through all glossary terms and find them in body text
 for gloss_el in gloss_terms:
-    gloss_term = strip_glossary_term(gloss_el) 
-    print 'Glossary Term:', gloss_term
-    found_term = find_term_in_text(term=gloss_term, 
-                                   gloss_file=gloss_file, 
-                                   search_dir=temp_dir_ls)
-    if found_term is not None:
-        text_term_el = found_term[0]
-        text_tree = found_term[1]
-        text_file = found_term[2]
+    gloss_term_list = strip_glossary_term(gloss_el)
+    for term in gloss_term_list:
+        print 'Glossary Term:', term
+        found_term = find_term_in_text(term=term,
+                                       gloss_file=gloss_file,
+                                       search_dir=temp_dir_ls)
+        if found_term is not None:
+            text_term_el = found_term[0]
+            text_tree = found_term[1]
+            text_file = found_term[2]
 
-        # add glossary reference hyperlink
-        add_child(parent=text_term_el, 
-                  element='a', 
-                  myclass='glossref', 
-                  myid='glossref_'+gloss_el.get('id'), 
-                  href=gloss_file+'#'+gloss_el.get('id'), 
-                  text='*',
-                  section = 'body'
-        )
-        save_html(temp_dir, text_file, text_tree ) # save chapter file
+            # add glossary reference hyperlink
+            add_child(parent=text_term_el, 
+                      element='a', 
+                      myclass='glossref', 
+                      myid='glossref_'+gloss_el.get('id'), 
+                      href=gloss_file+'#'+gloss_el.get('id'), 
+                      text='*',
+                      section = 'body'
+            )
+            save_html(temp_dir, text_file, text_tree ) # save chapter file
 
-        # add glossary term hyperlink
-        add_child(parent=(gloss_el.findall('.//h5'))[0], 
-                  element='a', 
-                  myclass='glossterm', 
-                  myid='glossterm_'+gloss_el.get('id'), 
-                  href=text_file+'#'+('glossref_'+gloss_el.get('id')), 
-                  text=u"Return",
-                  section = 'glossary'
-        ) 
+            # add glossary term hyperlink
+            add_child(parent=(gloss_el.findall('.//h5'))[0], 
+                      element='a', 
+                      myclass='glossterm', 
+                      myid='glossterm_'+gloss_el.get('id'), 
+                      href=text_file+'#'+('glossref_'+gloss_el.get('id')), 
+                      text=u"back",
+                      section = 'glossary'
+            ) 
 
 save_html(temp_dir, gloss_file, gloss_tree ) # save GLOSSARY FILE
 
